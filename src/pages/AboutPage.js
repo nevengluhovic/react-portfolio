680d39348bcb6aba78ffb90a044a4ba98963fc04
Neven Gluhovic <nevengluhovic@gmail.com>
2021-12-07 14:06:12 +0100

    3. Services Section

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 function AboutPage() {
   return (
     <div>
       <AboutSection />
+      <ServicesSection />
     </div>
   );
 }
