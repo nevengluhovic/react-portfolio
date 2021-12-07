acf8257a969d7a77f45c7b1de8cdf26ad65f3127
Neven Gluhovic <nevengluhovic@gmail.com>
2021-12-07 14:28:40 +0100

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function faqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            ipsam laborum rem esse sed officia! Dolores beatae pariatur magni
+            temporibus?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            ipsam laborum rem esse sed officia! Dolores beatae pariatur magni
+            temporibus?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Paying Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            ipsam laborum rem esse sed officia! Dolores beatae pariatur magni
+            temporibus?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            ipsam laborum rem esse sed officia! Dolores beatae pariatur magni
+            temporibus?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default faqSection;
